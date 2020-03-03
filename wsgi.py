db82a9bdf7404027d600b0aaebd99f776b2918dc
DrZoidberg <szymon.strus@gmail.com>
2020-03-03 22:07:36 +0100

    added img endpoint for image upload/download

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request, Response, send_file
+
 application = Flask(__name__)
 
-@application.route("/")
+
+@app.route('/img', methods=['POST', 'GET'])
+def saveImage():
+    if request.method == 'POST':
+        f = request.files['data']
+        f.save('recv.jpg')
+        application.logger.info("Saved file {}".format(f.filename))
+        return Response(response='OK', status=200)
+    elif request.method == 'GET':
+        return send_file('recv.jpg', 'image/jpeg')
+
+
+@app.route("/")
 def hello():
-    return "Hello World!"
+    return "File uploader"
+
 
 if __name__ == "__main__":
     application.run()
