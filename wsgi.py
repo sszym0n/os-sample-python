c2880bd1cce8bd99623b7a5a4691e220748cb782
DrZoidberg <szymon.strus@gmail.com>
2020-03-06 20:35:08 +0100

    json chunks instead of whole file

--- wsgi.py
+++ wsgi.py
@@ -7,34 +7,43 @@ import binascii
 application = Flask(__name__)
 
 
-@application.route('/img', methods=['POST', 'GET'])
-def saveImage():
+@application.route('/img/<station>', methods=['POST', 'GET'])
+def handle_image(station):
     if request.method == 'POST':
         f = request.files['data']
-        f.save('recv.jpg')
-        application.logger.info("Saved file {}".format(f.filename))
+        f.save(station + '.jpg')
+        application.logger.info("Data {}".format(request.headers))
         return Response(response='OK', status=200)
     elif request.method == 'GET':
-        return send_file('recv.jpg', 'image/jpeg')
+        if os.path.exists(station + '.jpg'):
+            return send_file(station + '.jpg', 'image/jpeg')
+        else:
+            return Response(response="No file for station", status=500)
 
 
 @application.route('/json', methods=['POST'])
-def jsonSave():
+def handle_json():
     json_data = json.loads(request.get_json())
 
     index = json_data["index"]
     num_of_chunks = json_data["num_of_chunks"]
+    file_name = json_data["station_name"] + ".jpg"
+    temp_file = json_data["station_name"] + ".tmp"
     buffer = binascii.a2b_base64(json_data["data"])
-    fileMode = 'ab'
-    if index == 0: fileMode = 'wb'
+    file_mode = 'ab'
+    if index == 0: file_mode = 'wb'
 
     try:
-        with open('out.tmp', fileMode) as file:
+        with open(temp_file, file_mode) as file:
             file.write(buffer)
 
         if index == num_of_chunks:
-            os.rename('out.tmp', 'out.jpg')
-    except OSError:
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            os.rename(temp_file, file_name)
+
+    except OSError as e:
+        application.logger.info(e)
         return Response(response='OSError', status=500)
 
     return Response(response='OK', status=200)
